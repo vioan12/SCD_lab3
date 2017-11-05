b4048ea1229e7a9b6e5ea70fbb90cd28a30fc431
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-11-06 01:07:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@ public class Main {
     private static Thread[] thread=new Thread[2];
     public static void main(String[] args) {
 
-        HtmlExtract HE = new HtmlExtract("http://www.cel.ro/casti/");
-        HE.extract_all_products();
+        HtmlExtractCel HEC = new HtmlExtractCel("http://www.cel.ro/casti/");
+        HEC.extract_all_products();
+
+        HtmlExtractEmag HEE= new HtmlExtractEmag("https://www.emag.ro/casti-pc/p1/c");
+        HEE.extract_all_products();
+
     }
 }
