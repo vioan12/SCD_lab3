2a077ca2804679fea96dd24016bf46983c1780ea
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-11-06 02:31:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 public class Main {
 
-    private static Thread[] thread=new Thread[2];
+    private static Thread[] thread=new Thread[1];
     public static void main(String[] args) {
 
-        HtmlExtractCel HEC = new HtmlExtractCel("http://www.cel.ro/casti/");
-        HEC.extract_all_products();
-
-        HtmlExtractEmag HEE= new HtmlExtractEmag("https://www.emag.ro/casti-pc/p1/c");
-        HEE.extract_all_products();
-
+        try {
+            for (int i = 0; i < thread.length; i++) {
+                thread[i] = new MainThread();
+            }
+            for (int i = 0; i < thread.length; i++) {
+                thread[i].start();
+            }
+            for (int i = 0; i < thread.length; i++) {
+                thread[i].join();
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
