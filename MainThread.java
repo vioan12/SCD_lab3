0b699e681c94f7a2ed896662d83429905e39c2b5
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-11-12 01:16:04 +0200

    Update MainThread.java

--- MainThread.java
+++ MainThread.java
@@ -1,42 +1,43 @@
+import org.jsoup.select.Elements;
+
+import java.util.Vector;
+
 /**
  * Created by ioan on 11/6/17.
  */
 public class MainThread extends Thread {
 
     private static boolean done = false;
-    private HtmlExtractCel HEC;
-    private HtmlExtractEmag HEE;
+    public Vector<Casca> Casti = new Vector<Casca>();
 
+    private Thread[] thread;
     MainThread()
     {
-        HEC = new HtmlExtractCel();
-        HEE= new HtmlExtractEmag();
+        thread = new Thread[121];
     }
 
     public void run()
     {
-        if (done == false) {
+        if (done == false){
+            try {
 
-            for(int i=1;i<=24;i++) {
-                HEC.Set_url("http://www.cel.ro/casti/0a-"+i);
-                HEC.extract_all_products();
-                try {
-                    this.sleep(30000);
-                } catch (Exception e) {
-                    System.err.println(e);
+                for (int i = 0; i < thread.length; i++) {
+                    if(i>10 && i<25)
+                        thread[i] = new MyThread("http://www.cel.ro/casti/0a-"+(i+1),null);
+                    if(i>=25)
+                        thread[i] = new MyThread(null,null);
+                    if(i<=10)
+                        thread[i] = new MyThread("http://www.cel.ro/casti/0a-"+(i+1),"https://www.emag.ro/casti-pc/p"+(i+1)+"/c");
+                }
+                for (int i = 0; i < thread.length; i++) {
+                    thread[i].start();
+                }
+                for(int i = 0; i < thread.length; i++){
+                    thread[i].join();
                 }
 
-            }
-
-
-            for(int i=1;i<=11;i++) {
-                HEE.Set_url("https://www.emag.ro/casti-pc/p"+i+"/c");
-                HEE.extract_all_products();
-                try {
-                    this.sleep(30000);
-                } catch (Exception e) {
+            } catch (Exception e) {
                     System.err.println(e);
-                }
             }
 
             setDone();
