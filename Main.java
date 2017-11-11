d1a5624a924fda20773080c7e9f75e5a9c7c1f5e
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-11-12 01:18:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+
 public class Main {
 
     private static Thread[] thread=new Thread[1];
     public static void main(String[] args) {
 
         try {
+
             for (int i = 0; i < thread.length; i++) {
                 thread[i] = new MainThread();
             }
@@ -13,6 +15,7 @@ public class Main {
             for (int i = 0; i < thread.length; i++) {
                 thread[i].join();
             }
+
         } catch (Exception e) {
             System.err.println(e);
         }
