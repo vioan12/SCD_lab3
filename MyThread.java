802bd3c6cbcc7af2f52cff8a63d92b822cc1206e
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-11-12 01:16:49 +0200

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -1,3 +1,5 @@
+import org.jsoup.select.Elements;
+
 import java.util.concurrent.locks.Lock;
 /**
  * Created by ioan on 10/21/17.
@@ -5,29 +7,38 @@ import java.util.concurrent.locks.Lock;
 
 public class MyThread extends Thread
 {
-    private Lock mutex = new LockDekker();
-    private static boolean done = false;
-    private String text;
-    MyThread(String value)
+    HtmlExtractCel HECel;
+    HtmlExtractEmag HEEmag;
+    private String cel_url;
+    private String emag_url;
+    MyThread(String value_cel, String value_emag)
     {
-        text=value;
+        HECel = new HtmlExtractCel();
+        HEEmag = new HtmlExtractEmag();
+        cel_url=value_cel;
+        emag_url=value_emag;
+        if(value_cel!=null)
+            HECel.Set_url(cel_url);
+        if(value_emag!=null)
+            HEEmag.Set_url(emag_url);
     }
 
 
     public void run()
     {
-        if (done == false) {
-            mutex.lock();
-            for(int i=0;i<20;i++)
-                System.out.println(text);
-            mutex.lock();
-            setDone();
+        try {
+
+            if(cel_url!=null) {
+                sleep(1000);
+                HECel.extract_all_products();
+            }
+            if(emag_url!=null) {
+                sleep(1000);
+                HEEmag.extract_all_products();
+            }
+
+        } catch (Exception e) {
+            System.err.println(e);
         }
     }
-
-    public void setDone()
-    {
-        done = true;
-    }
-
 }
