725d438210e5d1277025ac730a997843ef0dd76a
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-11-19 02:26:26 +0200

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -8,17 +8,21 @@ import java.util.concurrent.locks.Lock;
 
 public class MyThread extends Thread
 {
-    HtmlExtractCel HECel;
-    HtmlExtractEmag HEEmag;
+    private HtmlExtractCel HECel;
+    private HtmlExtractEmag HEEmag;
     private String cel_url;
     private String emag_url;
-    private Vector<Casca> ListaCasti_Emag,ListaCasti_Cel;
-    MyThread(String value_cel, String value_emag)
+    private Vector<Casca> SubListaCasti_Emag,SubListaCasti_Cel;
+    private int mythread_id;
+    private static boolean flag[] = new boolean[MainThread.NR_OF_THREADS];
+    private static int turn;
+    MyThread(String value_cel, String value_emag, Integer value_mythreadid)
     {
         HECel = new HtmlExtractCel();
         HEEmag = new HtmlExtractEmag();
         cel_url=value_cel;
         emag_url=value_emag;
+        mythread_id=value_mythreadid;
         if(value_cel!=null)
             HECel.Set_url(cel_url);
         if(value_emag!=null)
@@ -30,40 +34,48 @@ public class MyThread extends Thread
     {
         try {
 
-            int sw;
-
             if(cel_url!=null) {
                 sleep(2000);
-                ListaCasti_Cel=HECel.extract_all_products();
+                SubListaCasti_Cel=HECel.extract_all_products();
             }
 
             if(emag_url!=null) {
                 sleep(2000);
-                ListaCasti_Emag=HEEmag.extract_all_products();
+                SubListaCasti_Emag=HEEmag.extract_all_products();
             }
 
+            flag[mythread_id]=true;
+            for(int j=0;j<MainThread.NR_OF_THREADS;j++){
+                if(j!=mythread_id) {
+                    turn =j;
+                    while ((flag[j]==true) && (turn==j)){
+                    }
+                }
+            }
 
-            for(int i=0;i<ListaCasti_Cel.size();i++) {
-                sw=0;
+            //Sectiune critica - BEGIN
+            for(int i=0;i<SubListaCasti_Cel.size();i++) {
+                int sw=0;
                 for(int j=0;j<MainThread.ListaCasti.size();j++)
-                    if(ListaCasti_Cel.elementAt(i).Get_id().equals(MainThread.ListaCasti.elementAt(j).Get_id())) {
-                        MainThread.ListaCasti.elementAt(j).Add(ListaCasti_Cel.elementAt(i).Get_id(), ListaCasti_Cel.elementAt(i).Get_nume(), ListaCasti_Cel.elementAt(i).Get_pret1(), ListaCasti_Cel.elementAt(i).Get_url1());
+                    if(SubListaCasti_Cel.elementAt(i).Get_id().equals(MainThread.ListaCasti.elementAt(j).Get_id())) {
+                        MainThread.ListaCasti.elementAt(j).Add(SubListaCasti_Cel.elementAt(i).Get_id(), SubListaCasti_Cel.elementAt(i).Get_nume(), SubListaCasti_Cel.elementAt(i).Get_pret1(), SubListaCasti_Cel.elementAt(i).Get_url1());
                         sw = 1;
                     }
                 if(sw==0)
-                    MainThread.ListaCasti.add(ListaCasti_Cel.elementAt(i));
+                    MainThread.ListaCasti.add(SubListaCasti_Cel.elementAt(i));
             }
-
-            for(int i=0;i<ListaCasti_Emag.size();i++) {
-                sw=0;
+            for(int i=0;i<SubListaCasti_Emag.size();i++) {
+                int sw=0;
                 for(int j=0;j<MainThread.ListaCasti.size();j++)
-                    if(ListaCasti_Emag.elementAt(i).Get_id().equals(MainThread.ListaCasti.elementAt(j).Get_id())) {
-                        MainThread.ListaCasti.elementAt(j).Add(ListaCasti_Emag.elementAt(i).Get_id(), ListaCasti_Emag.elementAt(i).Get_nume(), ListaCasti_Emag.elementAt(i).Get_pret1(), ListaCasti_Emag.elementAt(i).Get_url1());
+                    if(SubListaCasti_Emag.elementAt(i).Get_id().equals(MainThread.ListaCasti.elementAt(j).Get_id())) {
+                        MainThread.ListaCasti.elementAt(j).Add(SubListaCasti_Emag.elementAt(i).Get_id(), SubListaCasti_Emag.elementAt(i).Get_nume(), SubListaCasti_Emag.elementAt(i).Get_pret1(), SubListaCasti_Emag.elementAt(i).Get_url1());
                         sw = 1;
                     }
                 if(sw==0)
-                    MainThread.ListaCasti.add(ListaCasti_Emag.elementAt(i));
+                    MainThread.ListaCasti.add(SubListaCasti_Emag.elementAt(i));
             }
+            flag[mythread_id]=false;
+            //Sectiune critica - END
 
         } catch (Exception e) {
             System.err.println(e);
